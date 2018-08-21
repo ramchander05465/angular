1f2f59afddacc36dbe53f31baa6b600a8af9e553
Ram.Chander <cram@netapp.com>
2018-08-21 17:42:13 +0530

    implemented two data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
@@ -10,7 +11,8 @@ import { DatabindingComponent } from './databinding/databinding.component';
     DatabindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
