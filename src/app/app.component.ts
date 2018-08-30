2c3184fd3ac0183bd3d99eba5b1b26f35252092c
Ram.Chander <cram@netapp.com>
2018-08-30 16:11:49 +0530

    implemented redux in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import {Post} from './post.model';
+import * as PostActions from './post.action';
+
+
+interface AppState {
+  message:string,
+  post:Post
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPOC';
+  textData:string="";
+  message$ : Observable<string>
+  post$:Observable<Post>
+
+  constructor(private store:Store<AppState>){
+    this.message$ = this.store.select('message');
+    this.post$ = this.store.select('post')
+  }
+
+  spanishMessage(){
+    this.store.dispatch({type:'Spanish'})
+  }
+
+  englishMessage(){
+    this.store.dispatch({type:'English'})
+  }
+
+  editText(){
+    this.store.dispatch(new PostActions.EditText(this.textData))
+  }
+
+  upVote(){
+    this.store.dispatch(new PostActions.Upvote())
+  }
+
+  downVote(){
+    this.store.dispatch(new PostActions.Downvote())
+  }
+
+  reset(){
+    this.store.dispatch(new PostActions.Reset()) 
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {StoreModule} from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import {simpleReducer} from './simple.reducer'; 
+import {postReducer} from './post.reducer'
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      message:simpleReducer,
+      post:postReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
