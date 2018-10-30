160f47dc1ea68d60b48098d6802ee11a63f093c8
Ram.Chander <cram@netapp.com>
2018-10-30 12:03:59 +0530

    updated some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  servers=[];
   title = 'angularPOC';
+
+  addServer  = () => {
+    this.servers.push('server')
+  }
+
+  onRemoveServer = (id: number) => {
+    const position = id+1;
+    this.servers.splice(position, 1)
+  }
 }
